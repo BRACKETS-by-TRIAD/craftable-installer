497d4e1512c45f0f2ca09b0907d3a420a133106b
David Běhal <behal.david@gmail.com>
2019-09-16 12:59:08 +0200

    Added laravle 6 support and refactored

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -2,12 +2,12 @@
 
 namespace Brackets\CraftableInstaller\Console;
 
-use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
 
 class NewCommand extends Command
 {
@@ -20,19 +20,21 @@ class NewCommand extends Command
     {
         $this
             ->setName('new')
-            ->setDescription('Create a new Craftable application using latest Laravel (currently 5.8).')
+            ->setDescription('Create a new Craftable application using latest Laravel (currently 6).')
             ->addArgument('name', InputArgument::OPTIONAL)
-            ->addOption('dev', null, InputOption::VALUE_NONE, 'Installs the latest DEV release ready for Craftable development')
-            ->addOption('lts', null, InputOption::VALUE_NONE, 'Installs Craftable using LTS release of Laravel (currently 5.5)')
-            ->addOption('no-install', null, InputOption::VALUE_NONE, 'Do not run craftable:install')
-            ;
+            ->addOption('dev', null, InputOption::VALUE_NONE,
+                'Installs the latest DEV release ready for Craftable development')
+            //Disabled for this version, we will need this later when first non LTS version of Laravel will be released
+//            ->addOption('lts', null, InputOption::VALUE_NONE, 'Installs Craftable using LTS release of Laravel (currently 5.5)')
+            ->addOption('v4', null, InputOption::VALUE_NONE, 'Installs Craftable using latest 5.8 release of Laravel')
+            ->addOption('no-install', null, InputOption::VALUE_NONE, 'Do not run craftable:install');
     }
 
     /**
      * Execute the command.
      *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param InputInterface $input
+     * @param OutputInterface $output
      * @return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -41,44 +43,44 @@ class NewCommand extends Command
 
         $commands = [];
 
-        $directory = "\"".$input->getArgument('name')."\"";
+        $directory = '"' . $input->getArgument('name') . '"';
 
-        array_push($commands, $composer.' create-project --prefer-dist laravel/laravel '.$directory.($input->getOption('lts') ? ' "5.5.*" ' : ' "5.8.*" '));
+        $commands[] = $composer . ' create-project --prefer-dist laravel/laravel ' . $directory . ($input->getOption('v4') ? ' "5.8" ' : ' "6.*" ');
 
-        array_push($commands, 'cd '.$directory);
+        $commands[] = 'cd ' . $directory;
 
         $output->writeln('<info>Crafting Craftable :) ...</info>');
 
         $packages = [
-            "brackets/admin-ui",
-            "brackets/admin-listing",
-            "brackets/admin-auth",
-            "brackets/admin-translations",
-            "brackets/media",
-            "brackets/translatable",
-            "brackets/advanced-logger",
-            "brackets/craftable",
+            'brackets/admin-ui',
+            'brackets/admin-listing',
+            'brackets/admin-auth',
+            'brackets/admin-translations',
+            'brackets/media',
+            'brackets/translatable',
+            'brackets/advanced-logger',
+            'brackets/craftable',
         ];
 
         if ($input->getOption('dev')) {
-            $packages = array_map(function($package) {
-                return '"'.$package.':dev-master"';
+            $packages = array_map(static function ($package) {
+                return '"' . $package . ':dev-master"';
             }, $packages);
-            array_push($commands, $composer.' require '.implode(' ', $packages));
-            array_push($commands, $composer.' require --dev "brackets/admin-generator:dev-master"');
-            array_push($commands, 'rm -rf vendor/brackets');
-            array_push($commands, $composer.' update --prefer-source');
+            $commands[] = $composer . ' require ' . implode(' ', $packages);
+            $commands[] = $composer . ' require --dev "brackets/admin-generator:dev-master"';
+            $commands[] = 'rm -rf vendor/brackets';
+            $commands[] = $composer . ' update --prefer-source';
         } else {
-            array_push($commands, $composer.' require "brackets/craftable"');
-            array_push($commands, $composer.' require --dev "brackets/admin-generator"');
+            $commands[] = $composer . ' require "brackets/craftable"';
+            $commands[] = $composer . ' require --dev "brackets/admin-generator"';
         }
 
         if (!$input->getOption('no-install')) {
             // FIXME these commands seem to not work on some environments (probably on some Windows platforms) when run this way (but they work when run manually) - this needs further investigation
-            array_push($commands, '"'.PHP_BINARY.'" artisan craftable:init-env');
-            array_push($commands, '"'.PHP_BINARY.'" artisan craftable:install');
-            array_push($commands, 'npm install');
-            array_push($commands, 'npm run dev');
+            $commands[] = '"' . PHP_BINARY . '" artisan craftable:init-env';
+            $commands[] = '"' . PHP_BINARY . '" artisan craftable:install';
+            $commands[] = 'npm install';
+            $commands[] = 'npm run dev';
 
         }
 
@@ -89,7 +91,7 @@ class NewCommand extends Command
             $process->setTty(true);
         }
 
-        $process->run(function ($type, $line) use ($output) {
+        $process->run(static function ($type, $line) use ($output) {
             $output->write($line);
         });
 
@@ -103,8 +105,8 @@ class NewCommand extends Command
      */
     protected function findComposer()
     {
-        if (file_exists(getcwd().'/composer.phar')) {
-            return '"'.PHP_BINARY.'" composer.phar';
+        if (file_exists(getcwd() . '/composer.phar')) {
+            return '"' . PHP_BINARY . '" composer.phar';
         }
 
         return 'composer';
